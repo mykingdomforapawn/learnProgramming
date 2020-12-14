18c849006a6d800b153cffbe48f82731c3c5eb03
Ludwig <ludwig.nachname.spam@gmail.com>
2020-12-14 15:48:28 +0100

    Introduced docstring documentation

---
+++ algorithmic_complexity/algorithm_potpourri.py
@@ -0,0 +1,21 @@
+"""Collection of simple algorithms to analyse the runtime with.
+
+This module should be imported to runtime_analysis in order to experiment and compare the runtime of different algorithms.
+"""
+
+
+def print_first_element(arr):
+    """Print first element of an array.
+
+    Parameters:
+        arr (int): End date for performance data.
+
+    Returns:
+        None
+
+    Raises:
+        None
+
+    """
+
+    print(arr[0])
---
+++ algorithmic_complexity/runtime_analysis.py
@@ -0,0 +1,6 @@
+"""Playground to experiment and compare the runtime of different algorithms.
+
+A collection of simple anlgorithms can be found in the module algorithm_potpourry.py and should be imported
+"""
+
+import runtime_analysis as ra
