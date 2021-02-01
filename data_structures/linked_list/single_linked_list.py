ac921f932bb7bd1f94542d91736b939603057000
Ludwig <ludwig.nachname.spam@gmail.com>
2021-02-01 17:08:43 +0100

    Added SingleLinkedList class

--- data_structures/linked_list/single_linked_list.py
+++ data_structures/linked_list/single_linked_list.py
@@ -17,8 +17,8 @@ class Node:
         Raises:
             None
         """
-        self.data_ = data
-        self.next_ = next
+        self.data = data
+        self.next = next
 
     def __str__(self):
         """Get string representation of the data from a node.
@@ -32,7 +32,7 @@ class Node:
         Raises:
             None
         """
-        return str(self.data_)
+        return str(self.data)
 
     def get_data(self):
         """Get data from a node object.
@@ -46,7 +46,7 @@ class Node:
         Raises:
             None
         """
-        return self.data_
+        return self.data
 
     def set_data(self, data):
         """Set data in a node object.
@@ -60,7 +60,7 @@ class Node:
         Raises:
             None
         """
-        self.data_ = data
+        self.data = data
 
     def get_next(self):
         """Get the next node object after this one.
@@ -74,7 +74,7 @@ class Node:
         Raises:
             None
         """
-        return self.next_
+        return self.next
 
     def set_next(self, next):
         """Set the reference to the next node object after this one.
@@ -88,4 +88,155 @@ class Node:
         Raises:
             None
         """
-        self.next_ = next
+        self.next = next
+
+
+class SingleLinkedList(object):
+    """Implemetation of a single linked list. """
+
+    def __init__(self):
+        """Init node object.
+
+        Parameters:
+            data (...): Data that is stored in this node
+            next (Node): Reference to the next node in the list
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
+        self.head = None
+        self.size = 0
+
+    def __len__(self):
+        """Get length of the linked list object.
+
+        Parameters:
+            None
+
+        Returns:
+            [...] (int): Number of data nodes in the list object
+
+        Raises:
+            None
+        """
+        return self.size
+
+# TODO: clear code if not needed
+        #count = 0
+        #current = self.head_
+        # while current:
+        #    count += 1
+        #    current = current.get_next()
+        # return count
+
+    def __str__(self):
+        """Get length of the linked list object.
+
+        Parameters:
+            None
+
+        Returns:
+            [...] (int): Number of data nodes in the list object
+
+        Raises:
+            None
+        """
+        current = self.head_
+        output = ""
+        while current:
+            output += str(current) + " -> "
+            current = current.get_next()
+        return output
+
+    def _increase_size(self):
+        pass
+
+    def _decrease_size(self):
+        pass
+
+    def set_head(self, head_node):
+        self.head_ = head_node
+
+    # Pops an item from the front of the list
+
+    def pop(self):
+        if self.head_:
+            self.head_ = self.head_.get_next()
+        else:
+            raise IndexError("Unable to pop from empty list")
+
+    # Returns true if list contains the given value.
+    def contains(self, value):
+        found = False
+        current = self.head_
+        while current and not found:
+            if current.get_data() == value:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    # Deletes all instances of given value in list.
+    def delete(self, value):
+        current = self.head_
+        prev = None
+        while current:
+            if current.get_data() == value:
+                if prev:
+                    prev.set_next(current.get_next())
+                else:
+                    self.head_ = current.get_next()
+            prev = current
+            current = current.get_next()
+
+    # Pushes an item on the front of the list.
+    def push(self, value):
+        node = Node(value)
+        node.set_next(self.head_)
+        self.set_head(node)
+
+    def append(self, value):
+        node = Node(value)
+        current = self.head_
+        if not current:
+            self.head_ = node
+            return
+
+        while current.get_next():
+            current = current.get_next()
+
+        current.set_next(node)
+
+
+def main():
+    ll = SingleLinkedList()
+
+    print("Initial size: ", len(ll))
+    ll.push(24)
+    print("New size: ", len(ll))
+    print("List content: ", ll)
+    print("Pushing more")
+    ll.push(6)
+    ll.push(783)
+    print("List content: ", ll)
+    print("popping...")
+    ll.pop()
+    print("List content: ", ll)
+    print("Does list contain 24?")
+    if ll.contains(24):
+        print("Yes")
+    else:
+        print("No")
+    print("Deleting 24")
+    ll.delete(24)
+    print("List content: ", ll)
+    print("Pushing another onto end.")
+    ll.append(365)
+    print("List content: ", ll)
+
+
+if __name__ == "__main__":
+    main()
